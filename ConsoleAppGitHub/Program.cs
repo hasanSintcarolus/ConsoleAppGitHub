9ed6a1c77c34a961fc9d994a168e7a9902607d60
hasanSintcarolus <hasan.gok@sint-carolus.be>
2025-03-19 13:59:54 +0100

    Add project files.

---
+++ ConsoleAppGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.writeLine("Hello Github! "); 
+
+        }
+    }
+}
